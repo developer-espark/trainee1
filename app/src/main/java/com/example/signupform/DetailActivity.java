c9f779f4ee6153019c043ddf86af292d5854db25
sarvesh <sarvesh.yadav@esparkbizmail.com>
2020-08-20 18:58:33 +0530

    final commit

--- app/src/main/java/com/example/signupform/DetailActivity.java
+++ app/src/main/java/com/example/signupform/DetailActivity.java
@@ -3,12 +3,33 @@ package com.example.signupform;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
-
+    TextView fn,ln,gd,em,pw;
+    String fname,lname,gender,email,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        fn=findViewById(R.id.showfname);
+        ln=findViewById(R.id.showlname);
+        gd=findViewById(R.id.showgender);
+        em=findViewById(R.id.showemail);
+        pw=findViewById(R.id.showpassword);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            fname = extras.getString("fname");
+            lname = extras.getString("lname");
+            gender = extras.getString("gender");
+            email = extras.getString("email");
+            password = extras.getString("password");
+        }
+        fn.setText(fname);
+        ln.setText(lname);
+        gd.setText(gender);
+        em.setText(email);
+        pw.setText(password);
     }
 }
--- app/src/main/java/com/example/signupform/MainActivity.java
+++ app/src/main/java/com/example/signupform/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.signupform;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -14,54 +15,50 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     Button signup;
-    EditText fname,lname,email,password;
+    EditText fname, lname, email, password;
     RadioGroup genderselect;
     RadioButton gender;
     Toast t1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        signup=findViewById(R.id.signupbtn);
-        fname=findViewById(R.id.fname);
-        lname=findViewById(R.id.lname);
-        email=findViewById(R.id.email);
-        password=findViewById(R.id.password);
-        genderselect=findViewById(R.id.genderselect);
+        signup = findViewById(R.id.signupbtn);
+        fname = findViewById(R.id.fname);
+        lname = findViewById(R.id.lname);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        genderselect = findViewById(R.id.genderselect);
 
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                    int genderid=genderselect.getCheckedRadioButtonId();
-                    gender=findViewById(genderid);
-//                    t1= Toast.makeText(MainActivity.this,gender.getText().toString(),Toast.LENGTH_LONG);
-//                    t1.show();
-                    if(TextUtils.isEmpty(fname.getText().toString())){
-                        t1= Toast.makeText(MainActivity.this,"Please Enter first Name",Toast.LENGTH_LONG);
-                        t1.show();
-                    }
-                    else if(TextUtils.isEmpty(lname.getText().toString())){
-                        t1= Toast.makeText(MainActivity.this,"Please Enter last Name",Toast.LENGTH_LONG);
-                        t1.show();
-                    }
-                    else if(TextUtils.isEmpty(email.getText().toString())){
-                        t1= Toast.makeText(MainActivity.this,"Please Enter email",Toast.LENGTH_LONG);
-                        t1.show();
-                    }
-                    else if(TextUtils.isEmpty(password.getText().toString())){
-                        t1= Toast.makeText(MainActivity.this,"Please Enter password",Toast.LENGTH_LONG);
-                        t1.show();
-                    }
-                    else if(gender.getText().toString()==null)
-                    {
-                        t1= Toast.makeText(MainActivity.this,"Please select gender",Toast.LENGTH_LONG);
-                        t1.show();
-                    }
-                    else{
-                        t1= Toast.makeText(MainActivity.this,fname.getText().toString(),Toast.LENGTH_LONG);
-                        t1.show();
-                    }
+                int genderid = genderselect.getCheckedRadioButtonId();
+                gender = findViewById(genderid);
+                if (TextUtils.isEmpty(fname.getText().toString())) {
+                    t1 = Toast.makeText(MainActivity.this, "Please Enter first Name", Toast.LENGTH_LONG);
+                    t1.show();
+                } else if (TextUtils.isEmpty(lname.getText().toString())) {
+                    t1 = Toast.makeText(MainActivity.this, "Please Enter last Name", Toast.LENGTH_LONG);
+                    t1.show();
+                } else if (TextUtils.isEmpty(email.getText().toString())) {
+                    t1 = Toast.makeText(MainActivity.this, "Please Enter email", Toast.LENGTH_LONG);
+                    t1.show();
+                } else if (TextUtils.isEmpty(password.getText().toString())) {
+                    t1 = Toast.makeText(MainActivity.this, "Please Enter password", Toast.LENGTH_LONG);
+                    t1.show();
+                } else {
+                    Intent i=new Intent();
+                    i.setClass(MainActivity.this,DetailActivity.class);
+                    i.putExtra("fname",fname.getText().toString());
+                    i.putExtra("lname",lname.getText().toString());
+                    i.putExtra("gender",gender.getText().toString());
+                    i.putExtra("email",email.getText().toString());
+                    i.putExtra("password",password.getText().toString());
+                    startActivity(i);
+                }
             }
         });
     }
